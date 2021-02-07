5c38c3cdf4de412e35c2f2d78c4cca7c9f329d3f
Alexander Choban <alexander.choban@gmail.com>
2021-02-07 11:08:58 -0600

    Added team shadow basic content

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,33 @@
 import styles from "../styles/Home.module.css";
 
-import Nav from '../components/Nav'
+import Nav from "../components/Nav";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
+    <div
+      style={{
+        background: "url(/images/header_2x.jpg)",
+        backgroundSize: "100% auto",
+      }}
+      className="h-screen"
+    >
       <Nav />
-      <div className="pt-8">
-      <div className="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
-        <div className="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left">
-          <p className="uppercase tracking-loose w-full">What business are you?</p>
-          <h1 className="my-4 text-5xl font-bold leading-tight">
-            Main Hero Message to sell yourself!
-          </h1>
-          <p className="leading-normal text-2xl mb-8">
-            Sub-hero message, not too long and not too short. Make it just right!
-          </p>
-          <button className="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-            Subscribe
-          </button>
+      <div className="pt-24">
+        <div className="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
+          <div className="flex flex-col w-full justify-center text-center items-start text-center md:text-left">
+            <h1 className="my-4 text-5xl font-bold leading-tight text-center mx-auto uppercase">
+              <span className="bg-blue-500">AUTOMOTIVE FUN</span>
+            </h1>
+            <p className="leading-normal text-2xl mb-8 text-center mx-auto">
+              <span className="bg-blue-500">
+                Team Shadow Racing is an online and real life car club. <br />
+                We strive to be the best.
+              </span>
+            </p>
+          </div>
         </div>
       </div>
     </div>
-    </>
   );
 }
